58c7b75a307a5771ce668c2184b8a0cf3eabf5e4
— <drcmda@gmail.com>
2023-04-21 13:48:31 +0200

    cleanup

--- example/src/components/Scene.jsx
+++ example/src/components/Scene.jsx
@@ -13,20 +13,17 @@ function Model() {
     mesh.current.rotation.y += delta / 2
   })
   return (
-    <>
-      <Center ref={mesh}>
-        <mesh
-          geometry={nodes.cube.geometry}
-          material={materials.base}
-          material-color={color}
-          scale={active ? 0.3 : 0.25}
-          onClick={(e) => (e.stopPropagation(), setActive(!active))}
-          onPointerOver={(e) => (e.stopPropagation(), setHover(true))}
-          onPointerOut={(e) => setHover(false)}
-        />
-      </Center>
-      <ContactShadows color={color} position={[0, -1.5, 0]} blur={3} opacity={0.75} />
-    </>
+    <Center ref={mesh}>
+      <mesh
+        geometry={nodes.cube.geometry}
+        material={materials.base}
+        material-color={color}
+        scale={active ? 0.3 : 0.25}
+        onClick={(e) => (e.stopPropagation(), setActive(!active))}
+        onPointerOver={(e) => (e.stopPropagation(), setHover(true))}
+        onPointerOut={(e) => setHover(false)}
+      />
+    </Center>
   )
 }
 
@@ -34,8 +31,9 @@ export default function App() {
   return (
     <>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[10, 10, 5]} />
       <Model />
+      <ContactShadows color={color} position={[0, -1.5, 0]} blur={3} opacity={0.75} />
       <Environment preset="city" />
       <CameraControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
     </>
