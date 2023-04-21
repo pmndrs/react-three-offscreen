fb7986694b0ceb7e636341e06d905c67d823bcb9
— <drcmda@gmail.com>
2023-04-21 12:58:57 +0200

    refactor example

--- example/src/components/App.js
+++ example/src/components/App.js
@@ -3,9 +3,9 @@ import { Canvas } from '@react-three/offscreen'
 
 // This is the fallback component that will be rendered on the main thread
 // This will happen on systems where OffscreenCanvas is not supported
-const Scene = React.lazy(() => import('./worker/Scene'))
+const Scene = React.lazy(() => import('./Scene'))
 
 export default function App() {
-  const [worker] = React.useState(() => new Worker(new URL('./worker/index.js', import.meta.url)))
+  const [worker] = React.useState(() => new Worker(new URL('./worker.js', import.meta.url)))
   return <Canvas camera={{ position: [0, 0, 20], fov: 25 }} worker={worker} fallback={<Scene />} />
 }
--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './components/App'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(<App />)
