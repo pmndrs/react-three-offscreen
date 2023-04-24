9583fd6d661ea34e6ea22aa2b30ebb3f07a0e82a
— <drcmda@gmail.com>
2023-04-24 12:21:55 +0200

    readme

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // https://github.com/vitejs/vite-plugin-react/issues/151
+  // https://stackoverflow.com/questions/73815639/how-to-use-jsx-in-a-web-worker-with-vite
   plugins: [react({ fastRefresh: false })],
   worker: {
     plugins: [react({ fastRefresh: false })],
