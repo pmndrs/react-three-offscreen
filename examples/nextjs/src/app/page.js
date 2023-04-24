e817241035a2206e55425025ada399f4440b6334
— <drcmda@gmail.com>
2023-04-24 12:47:34 +0200

    fix next

--- examples/nextjs/src/app/page.js
+++ examples/nextjs/src/app/page.js
@@ -1,15 +1,5 @@
-"use client"
+import dynamic from 'next/dynamic'
 
-import React, { lazy } from 'react'
-import { Canvas } from '@react-three/offscreen'
+const App = dynamic(() => import('@/components/App'), { ssr: false })
 
-// This is the fallback component that will be rendered on the main thread
-// This will happen on systems where OffscreenCanvas is not supported
-const Scene = lazy(() => import('@/components/Scene'))
-
-// This is the worker thread that will render the scene
-const worker = new Worker(new URL('@/components/worker.jsx', import.meta.url), { type: 'module' })
-
-export default function App() {  
-  return <Canvas camera={{ position: [0, 0, 10], fov: 25 }} worker={worker} fallback={<Scene />} />
-}
+export default App
