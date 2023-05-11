5d80454706479ab4f023938be73046bed5b3abaf
— <drcmda@gmail.com>
2023-05-11 11:06:08 +0200

    fix font urls

--- examples/pacman/src/index.jsx
+++ examples/pacman/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css'
+import './global.css'
 import App from './components/App'
 import Overlay from './components/Overlay'
 
